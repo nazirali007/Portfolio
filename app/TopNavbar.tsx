b8ff0a14d876096ba0a0d230d52c167d98688fb7
Nazir Ali Siddiqui <itsnazirali1010@gmail.com>
2025-01-30 01:22:08 +0530

    resume fille name change

--- app/TopNavbar.tsx
+++ app/TopNavbar.tsx
@@ -8,15 +8,7 @@ const TopNavbar = () => {
       <div className="flex justify-between items-center px-4">
         <h1 className="text-xl">My Portfolio</h1>
         <div>
-          {/* <a href="/resume.pdf" download="Nazir_Ali_Siddiqui_Resume.pdf">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Download Resume
-            </button>
-          </a> */}
-          <a
-            href="/Nazir_Ali_Siddiqui_Resume.pdf"
-            download="Nazir_Ali_Siddiqui_Resume.pdf"
-          >
+          <a href="/NazirResume.pdf" download="Nazir_Ali_Siddiqui_Resume.pdf">
             <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
               Download Resume
             </button>
